57104b39030772c2fd2ed565c8063632087c63cb
zhanghuan <zhanghuan0126>
2021-04-12 20:24:49 +0800

    test

---
+++ android/asr_plugin/src/main/java/com/zhanghuan/plugin/AsrPlugin.java
@@ -0,0 +1,20 @@
+package com.zhanghuan.plugin;
+
+import androidx.annotation.NonNull;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+
+/**
+ * @version : V1.0
+ * @Author : Zhang.Huan
+ * @Description : flutter channel
+ * @Program : android
+ * @Date : 2021-04-12 19:46
+ **/
+public class AsrPlugin implements MethodChannel.MethodCallHandler {
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+
+    }
+}
