f91ddbefce6516aa7cc2bb9e95e465b877014813
zhanghuan <zhanghuan0126>
2021-04-13 10:37:17 +0800

    test

--- android/app/src/main/java/com/zhanghuan/flutter_app/MainActivity.java
+++ android/app/src/main/java/com/zhanghuan/flutter_app/MainActivity.java
@@ -1,6 +1,21 @@
 package com.zhanghuan.flutter_app;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.zhanghuan.plugin.AsrPlugin;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerSelfPlugin();
+    }
+
+    private void registerSelfPlugin() {
+    }
 }
